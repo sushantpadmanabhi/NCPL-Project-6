e3a54d234fa4b3946d223f9faff61361074222a6
Sushant Padmanabhi <isushant.p@gmail.com>
2025-01-14 22:42:38 -0500

    storage account name changed

--- manage-resource.sh
+++ manage-resource.sh
@@ -11,7 +11,7 @@ fi
 
 RESOURCE_GROUP="NCPL-Project-6"
 LOCATION="centralindia"
-STORAGE_ACCOUNT_NAME="ncplstorageaccount"
+STORAGE_ACCOUNT_NAME="ncplprjt6strgacc"
 
 echo "Creating a Storage Account $STORAGE_ACCOUNT_NAME in Resource Group $RESOURCE_GROUP ..."
 
